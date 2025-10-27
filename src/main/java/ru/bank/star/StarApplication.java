e873c9b4f858c22cec4854898f4c131624a7ff70
pborisp <popovbs@gmail.com>
2025-10-28 01:02:30 +0300

    start + repository

---
+++ src/main/java/ru/bank/star/StarApplication.java
@@ -0,0 +1,13 @@
+package ru.bank.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarApplication.class, args);
+	}
+
+}
