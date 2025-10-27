e74ea7ea8e0f1b8eb78fb6ea6d3f6d1642067b81
pborisp <popovbs@gmail.com>
2025-10-28 01:26:17 +0300

    start + repository + DTO

--- src/main/java/ru/bank/star/service/RecommendationRuleSet.java
+++ src/main/java/ru/bank/star/service/RecommendationRuleSet.java
@@ -1,46 +1,5 @@
 package ru.bank.star.service;
 
-import java.util.UUID;
-
 public interface RecommendationRuleSet {
-package ru.bank.star;
-
-import java.util.UUID;
-
-    public class RecomendDTO {
-        private UUID id;
-        private String name;
-        private String textRecomendation;
-
-        public RecomendDTO(UUID id, String name, String textRecomendation) {
-            this.id = id;
-            this.name = name;
-            this.textRecomendation = textRecomendation;
-        }
-
-        public UUID getId() {
-            return id;
-        }
-
-        public void setId(UUID id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getTextRecomendation() {
-            return textRecomendation;
-        }
-
-        public void setTextRecomendation(String textRecomendation) {
-            this.textRecomendation = textRecomendation;
-        }
-    }
 
 }
